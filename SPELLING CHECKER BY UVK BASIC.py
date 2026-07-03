000dd5b9cbd1e62c65cd248fa7f7842a06bdf325
U.V.KARTHIK <126742893+CARKINGUVK63@users.noreply.github.com>
2026-07-03 17:21:59 +0530

    Add files via upload

---
+++ SPELLING CHECKER BY UVK BASIC.py
@@ -0,0 +1,9 @@
+print("=======SPELLING CHECKER======")
+print("Done By UVK ")
+word=("Car","Karthik","ASUS","Vivobook","Programming","car","hello","Python","Computer")
+user_write=input("ENTER YOUR WORD !!! :-")
+
+if user_write in word:
+    print(" The entered spelling is correct .")
+else:
+    print(" The entered spelling is incorrect !!!!")
